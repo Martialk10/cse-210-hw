e436c151bd8a779674564162a4027bb8ed1b36ec
Martialk10 <martialkn06@gmail.com>
2024-03-17 14:27:03 +0000

    commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Sandbox World!");
+
+public class WayToLearnX {
+    public static void Main() {
+
+
+         
+        List<string> langage = new List<string> ();
+
+        
+        langage.Add("Java");
+        langage.Add("Python");
+        langage.Add("C#");
+        langage.Add("PHP");
+        langage.Add("C++");
+        langage.Add("SQL");
+
+        Console.WriteLine("browse the list of languages:");
+        foreach(string item in langage) {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("browse the list of languages :");
+        for (int i = 0; i < langage.Count; i++) {
+            Console.WriteLine(langage[i]);
+        }
     }
 }
