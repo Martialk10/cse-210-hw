4e9567965c1f63345acc6480f0a8a5fd25470649
Martialk10 <martialkn06@gmail.com>
2024-03-31 19:43:55 +0000

    commit

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,89 +1,181 @@
-[18:42, 31/03/2024] MVRKO ANG&LS: using System;
-// as a stretch, I tried to keep a log fo how many times activities were performed. 
-// I tried to make sure that there were no prompts repeated. 
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        int breatheCount = 0;
-        int reflectCount = 0;
-        int listCount = 0;
+        List<Goal> goals = new List<Goal>();
+        int totalPoints = 0;
+        int choice;
 
-        // Console.WriteLine("Hello Develop04 World!");
-        Console.WriteLine("Welcome to the Mindfulness Program!");
-        Console.WriteLine();
-        bool running = true;
-        while (running)
+        while ((choice = DisplayMenu()) != 6)
         {
-            Console.WriteLine("Please select one of the following activities: ");
-            Console.WriteLine("1. Breathing Activity");
-            Console.WriteLine("2. Reflecting Activity");
-            Console.WriteLine("3. Listing Activity"…
-[18:45, 31/03/2024] MVRKO ANG&LS: using System;
-using System.Diagnostics;
-
-class Program
-{
-
-    static void Main(string[] args)
-    {
-        // This will clear the console
-        Console.Clear();
-        Console.Write("\n** Welcome to the Mindfulness Program ***\n");
-        //Call Choices
-        Choices choice = new Choices();
-        int seconds;
-
-        int action = 0;
-        while (action != 4)
-        {
-            // Ask for user input (1-4)
-            action = choice.UserChoice();
-            switch (action)
+            switch (choice)
             {
                 case 1:
-                    // Start Breathing Activity
-                    Console.Clear();
-                    BreathingActivity breathing = new BreathingActivity("Breathing", 0);
-                    breathing.GetActivityName();
-              …
-[18:59, 31/03/2024] MVRKO ANG&LS: using System;
-
-public class EternalGoal : Goal
-{
-    // Attributes
-    private string _type = "Eternal Goal:";
-    private bool _status;
-
-
-    // Constructors
-    public EternalGoal(string type, string name, string description, int points) : base(type, name, description, points)
-    {
-        _status = false;
-    }
-    public EternalGoal(string type, string name, string description, int points, bool status) : base(type, name, description, points)
-    {
-        _status = status;
+                    goals.Add(CreateNewGoal());
+                    break;
+                case 2:
+                    ListGoals(goals);
+                    break;
+                case 3:
+                    SaveGoals(goals);
+                    break;
+                case 4:
+                    goals = LoadGoals();
+                    break;
+                case 5:
+                    totalPoints += RecordEvent(goals);
+                    System.Console.WriteLine($"You now have {totalPoints} points.");
+                    break;
+                default:
+                    System.Console.WriteLine("Invalid option. Try again.");
+                    break;
+            }
+        }
     }
 
-    // Methods
+    static int DisplayMenu()
+    {
+        System.Console.WriteLine("\nEternal Question Menu: ");
+        System.Console.WriteLine("1. Create New Goal");
+        System.Console.WriteLine("2. List Goals");
+        System.Console.WriteLine("3. Save Goals");
+        System.Console.WriteLine("4. Load Goals");
+        System.Console.WriteLine("5. Record Event");
+        System.Console.WriteLine("6. Quit");
 
-    public override void ListGoal(int i)
-    {
-        Console.WriteLine($"{i}. [ ] {GetName()} ({GetDescription()})");
-    }
-    public override string SaveGoal()
-    {
-        return ($"{_type}; {GetName()}; {GetDescription()}; {GetPoints()}; {_status}");
-    }
-    public override string LoadGoal()
-    {
-        return ($"{_type}; {GetName()}; {GetDescription()}; {GetPoints()}; {_status}");
-    }
-      public override void RecordGoalEvent(List<Goal> goals)
-    {
-       Console.WriteLine($"Congratulations! You have earned {GetPoints()} points!");
+        System.Console.WriteLine("\nEnter your choice: ");
+        int.TryParse(Console.ReadLine(), out int choice);
+        return choice;
     }
 
+    static Goal CreateNewGoal()
+    {
+        System.Console.WriteLine("\nChoose the type of goal:");
+        System.Console.WriteLine("1. Simple Goal");
+        System.Console.WriteLine("2. Eternal Goal");
+        System.Console.WriteLine("3. Checklist Goal");
 
+        System.Console.WriteLine("Enter your choice: ");
+        int.TryParse(Console.ReadLine(), out int choice);
+        
+        System.Console.WriteLine("What is the name of your goal? ");
+        string name = Console.ReadLine();
+
+        System.Console.WriteLine("What is a short description of it? ");
+        string description = Console.ReadLine();
+
+        System.Console.WriteLine("What is the amount of points associated with this goal? ");
+        int.TryParse(Console.ReadLine(), out int points);
+
+        switch (choice)
+        {
+            case 1:
+                return new SimpleGoal(name, description, points);
+            
+            case 2:
+                return new EternalGoal(name, description, points);
+            case 3:
+                System.Console.WriteLine("How many times does this goal need to be accomplished for a bonus? ");
+                int.TryParse(Console.ReadLine(), out int goalCount);
+
+                System.Console.WriteLine("What is the bonus for accomplishing it that many times? ");
+                int.TryParse(Console.ReadLine(), out int bonus);
+
+                return new ChecklistGoal(name, description, points, goalCount, bonus);
+            default:
+                System.Console.WriteLine("Invalid option. Creating a Simple Goal by default.");
+                return new SimpleGoal(name, description, points);
+        }
+    }
+
+    static void ListGoals(List<Goal> goals)
+    {
+        System.Console.WriteLine("\nList of Goals: ");
+        foreach (Goal goal in goals)
+        {
+            System.Console.WriteLine(goal);
+        }
+    }
+
+    static void SaveGoals(List<Goal> goals)
+    {
+        System.Console.WriteLine("Enter the filename of the goal file: ");
+        string filename = Console.ReadLine();
+
+        using StreamWriter writer = new StreamWriter(filename);
+        foreach (Goal goal in goals)
+        {
+            writer.WriteLine(goal.GetType().Name);
+            writer.WriteLine(goal._name);
+            writer.WriteLine(goal._description);
+            writer.WriteLine(goal._points);
+            if (goal is ChecklistGoal checklistGoal)
+            {
+                writer.WriteLine(checklistGoal._goalCount);
+                writer.WriteLine(checklistGoal._bonus);
+            }
+        }
+    }
+
+    static List<Goal> LoadGoals()
+    {
+        System.Console.WriteLine("Enter the filename of the goal file: ");
+        string filename = Console.ReadLine();
+
+        List<Goal> goals = new List<Goal>();
+        using StreamReader reader = new StreamReader(filename);
+
+        string line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            string name = reader.ReadLine();
+            string description = reader.ReadLine();
+            int.TryParse(reader.ReadLine(), out int points);
+
+            switch (line)
+            {
+                case nameof(SimpleGoal):
+                    goals.Add(new SimpleGoal(name, description, points));
+                    break;
+                case nameof(EternalGoal):
+                    goals.Add(new EternalGoal(name, description, points));
+                    break;
+                case nameof(ChecklistGoal):
+                    int.TryParse(reader.ReadLine(), out int goalCount);
+                    int.TryParse(reader.ReadLine(), out int bonus);
+                    goals.Add(new ChecklistGoal(name, description, points, goalCount, bonus));
+                    break;
+            }
+        }
+        return goals;
+    }
+
+    static int RecordEvent(List<Goal> goals)
+    {
+        System.Console.WriteLine("\nChoose the goal you want to record an event for: ");
+        for (int i = 0; i < goals.Count; i++)
+        {
+            System.Console.WriteLine($"{i + 1}. {goals[i]}");
+        }
+
+        System.Console.WriteLine("Enter your choice: ");
+        int.TryParse(Console.ReadLine(), out int choice);
+
+        if (choice > 0 && choice <= goals.Count)
+        {
+            Goal selectedGoal = goals[choice - 1];
+            selectedGoal.RecordEvent();
+            System.Console.WriteLine($"Event recorded for goal: {selectedGoal._name}");
+            return selectedGoal._completed ? selectedGoal._points : 0;
+        }
+        else
+        {
+            System.Console.WriteLine("Invalid option. No event recorded.");
+            return 0;
+        }
+    }
+}
